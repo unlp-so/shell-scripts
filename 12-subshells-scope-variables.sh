6f72d55e99d49d06c9d7a9e7ce8969a2f3a9b3af
Maximo Zarza <zmaximo1990@gmail.com>
2016-03-29 19:02:23 -0300

    Cambio de '_' por '-'. Agregado de ejmplo 12 sobre subshells

---
+++ 12-subshells-scope-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Este ejemplo muestra el comportamiento del alcance de las variables con subshells de por medio.
+
+i=0
+function f1()
+{
+    let i++
+    echo "In f1, SUBSHELL: $BASH_SUBSHELL, i=$i" >&2
+}
+
+f1
+f1 | f1 | f1
+
+echo "at end, i=$i"
